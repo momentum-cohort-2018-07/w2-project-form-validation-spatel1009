a86b53cde7fb0ae50ed6e015dd7435848d2c58b3
spatel1009 <spatel1009@gmail.com>
2018-08-09 15:17:41 -0400

    first step

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Add validation!');
+// block form from submitting if any fields are empty
+
+document.getElementById('parking-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    var field = document.getElementById('name').value.trim()
+
+    if (field === "") {
+       showEmptyError ()
+    } 
+})
+
+function showEmptyError () {
+    var errorDiv = document.createElement('div')
+    errorDiv.classList.add('error-msg')
+    errorDiv.innerText = 'All fields must be entered'
+}
