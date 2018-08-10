4ae0ef23c9f93ccd4b1b316518172312f39b6467
spatel1009 <spatel1009@gmail.com>
2018-08-10 11:43:48 -0400

    pre for loop

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 console.log('Add validation!');
 // block form from submitting if any fields are empty
 
-document.getElementById('parking-form').addEventListener('submit', function(event) {
+document.getElementById('parking-form').addEventListener('submit', function (event) {
     event.preventDefault()
 
-    var field = document.getElementById('name').value.trim()
+    var name = document.getElementById('name').value.trim()
 
-    if (field === "") {
-       showEmptyError ()
-    } 
+    if (name === "") {
+        showEmptyError()
+    } else if (name !== "") {
+        fieldPopulate()
+    }
+
+    var carY = document.getElementById('car-year').value.trim()
+
+    if (carY === "") {
+        showEmptyErrorY()
+    } else if (carY !== ""){
+        fieldPopulateY()
+    }
 })
 
-function showEmptyError () {
-    var errorDiv = document.createElement('div')
-    errorDiv.classList.add('error-msg')
-    errorDiv.innerText = 'All fields must be entered'
+
+
+function showEmptyError() {
+    var field = document.getElementById('name-field')
+    field.classList.add('input-invalid')
+}
+
+function fieldPopulate() {
+    var field = document.getElementById('name-field')
+    field.classList.add('input-valid')
+    field.classList.remove('errorDiv')
+}
+
+function showEmptyErrorY() {
+    var test = document.getElementById('car-field')
+    test.classList.add('input-invalid')
+}
+
+function fieldPopulateY() {
+    var test = document.getElementById('car-field')
+    test.classList.add('input-valid')
 }
