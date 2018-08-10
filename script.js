c49e540cb34fa2d729ec2d49418a496f1b2f920b
spatel1009 <spatel1009@gmail.com>
2018-08-10 13:52:46 -0400

    finished step 1

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById('parking-form').addEventListener('submit', function (eve
 
     if (name === "") {
         showEmptyError()
-    } else if (name !== "") {
+    } else {
         fieldPopulate()
     }
 
@@ -16,14 +16,68 @@ document.getElementById('parking-form').addEventListener('submit', function (eve
 
     if (carY === "") {
         showEmptyErrorY()
-    } else if (carY !== ""){
+    } else{
         fieldPopulateY()
+    }
+
+    var carMa = document.getElementById('car-make').value.trim()
+
+    if (carMa === "") {
+        showEmptyErrorY()
+    } else {
+        fieldPopulateY
+    }
+
+    var carMo = document.getElementById('car-model').value.trim()
+
+    if (carMo ===""){
+        showEmptyErrorY()
+    } else {
+        fieldPopulateY
+    }
+
+    var date = document.getElementById('start-date').value.trim()
+
+    if (date === "") {
+        showEmptyErrorDate()
+    } else {
+        fieldPopulateDate()
+    }
+
+    var days = document.getElementById('days').value.trim()
+
+    if (days ==="") {
+        showEmptyErrorDays()
+    } else {
+        fieldPopulateDays()
+    }
+
+    var cc = document.getElementById('credit-card').value.trim()
+
+    if (cc ==="") {
+        showEmptyErrorCC()
+    } else {
+        fieldPopulateCC()
+    }
+
+    var CVV = document.getElementById('cvv').value.trim()
+
+    if (CVV ==="") {
+        showEmptyErrorCVV()
+    } else {
+        fieldPopulateCVV()
+    }
+
+    var exp = document.getElementById('expiration').value.trim()
+
+    if (exp ==="") {
+        showEmptyErrorExp()
+    } else {
+        fieldPopulateExp()
     }
 })
 
-
-
-function showEmptyError() {
+function showEmptyError(field,id) {
     var field = document.getElementById('name-field')
     field.classList.add('input-invalid')
 }
@@ -31,7 +85,6 @@ function showEmptyError() {
 function fieldPopulate() {
     var field = document.getElementById('name-field')
     field.classList.add('input-valid')
-    field.classList.remove('errorDiv')
 }
 
 function showEmptyErrorY() {
@@ -42,4 +95,54 @@ function showEmptyErrorY() {
 function fieldPopulateY() {
     var test = document.getElementById('car-field')
     test.classList.add('input-valid')
+}
+
+function showEmptyErrorDate(){
+    var field = document.getElementById('start-date-field')
+    field.classList.add('input-invalid')
+}
+
+function fieldPopulateDate(){
+    var field = document.getElementById('start-date-field')
+    field.classList.add('input-valid')
+}
+
+function showEmptyErrorDays() {
+    var field = document.getElementById('days-field')
+    field.classList.add('input-invalid')
+}
+
+function fieldPopulateDays() {
+    var field = document.getElementById('days-field')
+    field.classList.add('input-valid')
+}
+
+function fieldPopulateCC() {
+    var field = document.getElementById('credit-card-field')
+    field.classList.add('input-valid')
+}
+
+function showEmptyErrorCC() {
+    var field = document.getElementById('credit-card-field')
+    field.classList.add('input-invalid')
+}
+
+function fieldPopulateCVV() {
+    var field = document.getElementById('cvv-field')
+    field.classList.add('input-valid')
+}
+
+function showEmptyErrorCVV() {
+    var field = document.getElementById('cvv-field')
+    field.classList.add('input-invalid')
+}
+
+function showEmptyErrorExp() {
+    var field = document.getElementById('expiration-field')
+    field.classList.add('input-invalid')
+}
+
+function fieldPopulateExp() {
+    var field = document.getElementById('expiration-field')
+    field.classList.add('input-valid')
 }
